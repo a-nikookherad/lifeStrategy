1ad95c2debab862ff69a6cc3ba199063539ed0e2
Ali Niko Kherad <alinikokherad@gmail.com>
2019-05-21 00:28:34 +0430

    create webpack mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -8,8 +8,23 @@ const mix = require('laravel-mix');
  | Mix provides a clean, fluent API for defining some Webpack build steps
  | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
- |
+ |.sass('resources/sass/app.scss', 'public/css')
  */
-
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.js('resources/js/main.js', 'resources/js/allInOne.js');
+/* mix.scripts([
+   'resources/js/myFiles/jquery.min.js',
+    'resources/js/myFiles/bootstrap.bundle.min.js',
+    'resources/js/myFiles/select2.min.js',
+    'resources/js/myFiles/sweetalert.min.js',
+    'resources/js/myFiles/all.min.js',
+    'resources/js/main.js'],
+    'public/js/main.min.js');*/
+// mix.sass('resources/sass/app.scss', 'public/css');
+/*mix.styles([
+    'resources/sass/myFiles/bootstrap.min.css',
+    'resources/sass/myFiles/bootstrap-rtl.min.css',
+    'resources/sass/myFiles/bootstrap-select.min.css',
+    'resources/sass/myFiles/all.min.css',
+    'resources/sass/app.scss',
+],
+    'public/css/main.min.css');*/
